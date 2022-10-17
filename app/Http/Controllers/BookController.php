5f3721b2f732e724965e727d7ac14dbd4981d58a
Kigamekun <reksa.prayoga1012@gmail.com>
2022-10-17 13:33:45 +0700

    feat(First)

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Redirect;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::latest()->paginate(10);
+
+        return Inertia::render('Book/Index', ['books' => $books]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Post::create(
+            Request::validate([
+                'title' => ['required', 'max:90'],
+                'description' => ['required'],
+            ])
+        );
+
+        return Redirect::route('posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Book $book)
+    {
+        $data = Request::validate([
+            'title' => ['required', 'max:90'],
+            'description' => ['required'],
+        ]);
+        $post->update($data);
+
+
+        return Redirect::route('posts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book)
+    {
+        $book->delete();
+
+        return Redirect::route('books.index');
+    }
+}
---
+++ resources/js/Pages/Book/Index.vue
@@ -0,0 +1,117 @@
+<template>
+    <Head title="Dashboard" />
+
+    <BreezeAuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
+                Post
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        <button class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="defaultModal">
+  Toggle modal
+</button>
+
+                        <table style="width: 100%">
+                            <thead class="font-bold bg-gray-300 border-b-2">
+                                <td class="px-4 py-2">ID</td>
+                                <td class="px-4 py-2">Title</td>
+                                <td class="px-4 py-2">Author</td>
+                                <td class="px-4 py-2">Publisher</td>
+                                <td class="px-4 py-2">Action</td>
+                            </thead>
+                            <tbody>
+                                <tr v-for="post in books.data" :key="post.id">
+                                    <td class="px-4 py-2">{{ post.id }}</td>
+                                    <td class="px-4 py-2">{{ post.title }}</td>
+                                    <td class="px-4 py-2">
+                                        {{ post.pengarang }}
+                                    </td>
+                                    <td class="px-4 py-2">
+                                        {{ post.penerbit }}
+                                    </td>
+                                    <td class="px-4 py-2 font-extrabold">
+                                        <Link
+                                            class="text-green-700"
+                                            :href="route('books.edit', post.id)"
+                                        >
+                                            Edit
+                                        </Link>
+                                        <Link
+                                            @click="destroy(post.id)"
+                                            class="text-red-700"
+                                            >Delete</Link
+                                        >
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <pagination :links="books.links" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+
+
+    <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+    <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Terms of Service
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="p-6 space-y-6">
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                </p>
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                </p>
+            </div>
+            <!-- Modal footer -->
+            <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                <button data-modal-toggle="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                <button data-modal-toggle="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import BreezeAuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import BreezeNavLink from "@/Components/NavLink.vue";
+import { Head } from "@inertiajs/inertia-vue3";
+import { Link } from "@inertiajs/inertia-vue3";
+export default {
+    components: {
+        BreezeAuthenticatedLayout,
+        Head,
+        BreezeNavLink,
+        Link,
+    },
+    props: {
+        books: Object,
+    },
+    methods: {
+        destroy(id) {
+            this.$inertia.delete(route("books.destroy", id));
+        },
+    },
+};
+</script>
+
+Laravel 8 CRUD with Inertia Js Using Vue 3 v1
